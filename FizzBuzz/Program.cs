0c4927c5937352857a557dbcfb4682419e7c7b23
Elin Nyman <elin.nyman87@gmail.com>
2026-09-21 16:51:08 +0200

    Complete FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,1 +1,40 @@
-﻿// According to Wikipedia, FizzBuzz is a group word game for children to teach them about division. This may or may not be true, but this question is generally used to torture screen young computer science graduates during programming interviews. Basically, this is how it works: you print the integers from 1 to 𝑁 , replacing any of them divisible by 𝑋 with Fizz or, if they are divisible by 𝑌 , with Buzz. If the number is divisible by both 𝑋 and 𝑌 , you print FizzBuzz instead. Check the samples for further clarification. Input Input contains a single test case. Each test case contains three integers on a single line, 𝑋 , 𝑌 and 𝑁 ( 1 ≤ 𝑋 < 𝑌 ≤ 𝑁 ≤ 100 ). Output Print integers from 1 to 𝑁 in order, each on its own line, replacing the ones divisible by 𝑋 with Fizz, the ones divisible by 𝑌 with Buzz and ones divisible by both 𝑋 and 𝑌 with FizzBuzz.
+﻿// According to Wikipedia, FizzBuzz is a group word game for children to teach them about division. 
+// This may or may not be true, but this question is generally used to torture screen young computer science graduates during programming interviews. 
+// Basically, this is how it works: you print the integers from 1 to 𝑁 , replacing any of them divisible by 𝑋 with Fizz or, if they are divisible by 𝑌 , with Buzz. 
+// If the number is divisible by both 𝑋 and 𝑌 , you print FizzBuzz instead. Check the samples for further clarification. 
+
+// Input
+// Input contains a single test case. Each test case contains three integers on a single line, 𝑋 , 𝑌 and 𝑁 ( 1 ≤ 𝑋 < 𝑌 ≤ 𝑁 ≤ 100 ).
+
+// Output 
+// Print integers from 1 to 𝑁 in order, each on its own line, replacing the ones divisible by 𝑋 with Fizz, the ones divisible by 𝑌 with Buzz and ones divisible by both 𝑋 and 𝑌 with FizzBuzz.
+
+static void FizzBuzz(int dividedX, int dividedY, int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    if (i % dividedX == 0)
+    {
+      if (i % dividedY == 0)
+      {
+        Console.WriteLine("FizzBuzz");
+      }
+      else
+      {
+        Console.WriteLine("Fizz");
+      }
+
+    }
+    else if (i % dividedY == 0)
+    {
+      Console.WriteLine("Buzz");
+    }
+    else
+    {
+      Console.WriteLine(i);
+    }
+  }
+}
+
+FizzBuzz(3, 5, 100);
+FizzBuzz(6, 9, 100);
--- FizzBuzz/obj/Debug/net10.0/FizzBuzz.AssemblyInfo.cs
+++ FizzBuzz/obj/Debug/net10.0/FizzBuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c5bc8b96ca025d80e495761de3e5929d1b46b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c11a05213986a608c1d23d779dec45d2cc7c442")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
