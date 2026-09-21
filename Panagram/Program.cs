0c11a05213986a608c1d23d779dec45d2cc7c442
Elin Nyman <elin.nyman87@gmail.com>
2026-09-21 16:33:41 +0200

    Add swedish pangram function aswell

--- Panagram/Program.cs
+++ Panagram/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Jag blev så osäker på funktionen för panagram så jag var tvungen att återskapa den
 // Tacka gudarna att jag har gjort denna övning för bara ett tag sedan XD
 
+// Och så kan jag inte döpa det rätt! Pangram inte Panagram! XD
+
 var text1 = "The quick brown fox jumps over the lazy dog";
 var text2 = "Hello, World!";
 
@@ -20,3 +22,44 @@ bool IsPanagram(string text)
 
 Console.WriteLine(IsPanagram(text1));
 Console.WriteLine(IsPanagram(text2));
+
+List<char> FindASwedishPanagram(string text)
+{
+  string swedishAlphabet = "abcdefghijklmnopqrstuvwxyzåäö";
+  var missingLetters = new List<char>();
+  var lowercaseText = text.ToLower();
+
+  foreach (char letter in swedishAlphabet)
+  {
+    if (!lowercaseText.Contains(letter))
+    {
+      missingLetters.Add(letter);
+    }
+  }
+
+  return missingLetters;
+}
+
+bool IsPanagramSwedish(string text)
+{
+  string swedishAlphabet = "abcdefghijklmnopqrstuvwxyzåäö";
+  var lowercaseText = text.ToLower();
+
+  foreach (char letter in swedishAlphabet)
+  {
+    if (!lowercaseText.Contains(letter))
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+foreach (char character in FindASwedishPanagram("Yxmördaren blåste på en hård frusen säckpipa"))
+{
+  Console.Write(character);
+}
+
+Console.WriteLine();
+Console.WriteLine(IsPanagramSwedish("Yxmördaren blåste på en hård frusen säckpipa gjoqvwz")); // <=== HAHAHAHAHAHAHA!
--- Panagram/obj/Debug/net10.0/Panagram.AssemblyInfo.cs
+++ Panagram/obj/Debug/net10.0/Panagram.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Panagram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2614c87dd5cff940e17df8bf1f3263dd8d6f7767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4311e2764796ea7c1a35868cab9a11b6c6feeeff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Panagram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Panagram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
