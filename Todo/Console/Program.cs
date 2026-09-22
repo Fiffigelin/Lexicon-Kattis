a3447556b0308bcac64a8cae4d5a0c1020367806
Elin Nyman <elin.nyman87@gmail.com>
2026-09-22 10:29:31 +0200

    Show all todos

--- Todo/Console/Program.cs
+++ Todo/Console/Program.cs
@@ -25,7 +25,7 @@ static void Header()
   Console.ResetColor();
 }
 
-static void MainMenu()
+void MainMenu()
 {
   Header();
   var options = new[]
@@ -52,7 +52,7 @@ static void MainMenu()
       AddTodo();
       break;
     case "2":
-      Console.WriteLine("Visar todos");
+      ShowTodos();
       break;
     case "3":
       Environment.Exit(0);
@@ -60,14 +60,19 @@ static void MainMenu()
   }
 }
 
-static void AddTodo()
+void AddTodo()
 {
   Header();
+  Console.WriteLine();
+  Console.ForegroundColor = ConsoleColor.DarkGray;
+  Console.WriteLine(" Skapa ny todo");
+  Console.ResetColor();
+
   Console.Write("Namn: ");
   var name = Console.ReadLine();
-  Console.WriteLine();
   Console.Write("Beskrivning: ");
   var desc = Console.ReadLine();
+  Console.WriteLine();
 
   var options = new[]
   {
@@ -91,8 +96,14 @@ static void AddTodo()
     switch (val)
     {
       case "1":
+        var todo = new Todo(name: name, desc: desc);
+        todos.Add(todo);
         Console.WriteLine("En todo har skapats");
-        Console.Write("Tryck ENTER för att komma tillbaka till startsidan");
+        Console.Write("Tryck ");
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.Write("ENTER ");
+        Console.ResetColor();
+        Console.Write("för att komma tillbaka till startsidan");
         Console.ReadLine();
         MainMenu();
         return;
@@ -105,5 +116,73 @@ static void AddTodo()
         val = Console.ReadLine();
         break;
     }
+  }
+}
+
+void ShowTodos()
+{
+  Header();
+
+  foreach (Todo todo in todos)
+  {
+    BorderLine();
+    Console.WriteLine(todo.Name);
+    Console.WriteLine(todo.Description);
+    ShowTaskStatus(todo.IsDone);
+    BorderLine();
+  }
+
+  var options = new[]
+  {
+    ("[1] Öppna todo", ConsoleColor.White),
+    ("[2] Backa", ConsoleColor.DarkGray),
+  };
+
+  foreach (var (op, color) in options)
+  {
+    Console.ForegroundColor = color;
+    Console.WriteLine(op);
+    Console.ResetColor();
+  }
+
+  Console.WriteLine();
+  Console.Write("Välj: ");
+  var val = Console.ReadLine();
+
+  while (true)
+  {
+    switch (val)
+    {
+      case "1":
+        // Skapa en funktion som visar alla val.
+        break;
+      case "2":
+        MainMenu();
+        return;
+      default:
+        Console.WriteLine("Ogiltigt val");
+        Console.Write("Välj: ");
+        val = Console.ReadLine();
+        break;
+    }
+  }
+}
+
+void BorderLine()
+{
+  Console.ForegroundColor = ConsoleColor.DarkGray;
+  Console.WriteLine("----------------------------");
+  Console.ResetColor();
+}
+
+void ShowTaskStatus(bool taskStatus)
+{
+  if (taskStatus)
+  {
+    Console.WriteLine("🗹");
+  }
+  else
+  {
+    Console.WriteLine("◻");
   }
 }
